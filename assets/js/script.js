1c814aeb30c75539cd7adc944a09cf994a5536a7
Ryan Raposo <raposo.ryan@gmail.com>
2022-06-17 16:10:34 -0400

    matchup payloads w/ A Clockwork Orange

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,32 @@
 const bookPayload = {
-    title: 'THE ALCHEMIST',
-    allowAnonLogging: false,
-    authors: ['PAULO COELHO'],
-    averageRating: 5,
-    canonicalVolumeLink: 'https://books.google.com/books/about/THE_ALCHEMIST.html?hl=&id=Kc9CiaS9q_wC',
-    contentVersion: '1.2.1.0.preview.0',
-    imageLinks: {smallThumbnail: 'http://books.google.com/books/content?id=…c=frontcover&img=1&zoom=5&source=gbs_api', thumbnail: 'http://books.google.com/books/content?id=Kc…tsec=frontcover&img=1&zoom=1&source=gbs_api'},
-    infoLink: 'http://books.google.ca/books?id=Kc9CiaS9q_wC&dq=the+alchemist&hl=&source=gbs_api',
+    allowAnonLogging: true,
+    authors: ['Anthony Burgess'],
+    averageRating: 4.5,
+    canonicalVolumeLink: 'https://play.google.com/store/books/details?id=qUI8pbpCNJUC',
+    categories: ['Fiction'],
+    contentVersion: '1.8.9.0.preview.2',
+    description: 'In this nightmare vision of a not-too-distant future, fifteen-year-old Alex and his three friends rob, rape, torture and murder - for fun. Alex is jailed for his vicious crimes and the State undertakes to reform him - but how and at what cost?',
+    imageLinks: {smallThumbnail: 'http://books.google.com/books/content?id=…er&img=1&zoom=5&edge=curl&source=gbs_api', thumbnail: 'http://books.google.com/books/content?id=qU…cover&img=1&zoom=1&edge=curl&source=gbs_api'},
+    infoLink: 'https://play.google.com/store/books/details?id=qUI8pbpCNJUC&source=gbs_api',
     language: 'en',
     maturityRating: 'NOT_MATURE',
+    pageCount: 176,
     panelizationSummary: {containsEpubBubbles: false, containsImageBubbles: false},
-    previewLink: 'http://books.google.ca/books?id=Kc9CiaS9q_wC&q=the+alchemist&dq=the+alchemist&hl=&cd=1&source=gbs_api',
+    previewLink: 'http://books.google.ca/books?id=qUI8pbpCNJUC&printsec=frontcover&dq=A+Clockwork+Orange&hl=&cd=1&source=gbs_api',
     printType: 'BOOK',
-    ratingsCount: 1,
-    readingModes: {text: false, image: false}
+    publishedDate: '2011-08-04',
+    publisher: 'Penguin UK'    
 }
 
 const moviePayload = {
-    "id": "tt2313197",
-    "title": "Batman: The Dark Knight Returns, Part 1",
-    "year": 2012,
-    "score": 84,
-    "type": "movie",
-    "imdbid": "tt2313197",
-    "tmdbid": 123025,
-    "traktid": 84956
+    id: 'tt0066921',
+    imdbid: 'tt0066921',
+    score: 79,
+    title: 'A Clockwork Orange',
+    tmdbid: 185,
+    traktid: 149,
+    type: 'movie',
+    year: 1971
 };
 
 const mdblistOptions = {
@@ -54,7 +56,7 @@ function searchTitle(title) {
 
 }
 
-searchTitle("batman");
+searchTitle("A Clockwork Orange");
 
 
 
