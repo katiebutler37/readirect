ed4e635cb30ebde18bf100891febc03f30f3b669
Ryan Raposo <raposo.ryan@gmail.com>
2022-06-20 17:02:49 -0400

    add payloads to own file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Switch this to false when you want to use real fetching, true when you want to use the mock payloads
-const DEBUG = false;
+const DEBUG = true;
 
 // Our Movie card variables which will hold the fetched data
 var movieCover = document.querySelector(".movie-cover");
@@ -19,68 +19,6 @@ var searchFormEl = document.querySelector(".search-form");
 var searchedTitleEl = document.querySelector("#searched-title");
 var resultsContainerEl = document.querySelector("#results");
 var searchHistoryContainerEl = document.querySelector('.search-history-items')
-
-const bookPayload = {
-    allowAnonLogging: true,
-    authors: ['Anthony Burgess'],
-    averageRating: 4.5,
-    canonicalVolumeLink: 'https://play.google.com/store/books/details?id=qUI8pbpCNJUC',
-    categories: ['Fiction'],
-    contentVersion: '1.8.9.0.preview.2',
-    description: 'In this nightmare vision of a not-too-distant future, fifteen-year-old Alex and his three friends rob, rape, torture and murder - for fun. Alex is jailed for his vicious crimes and the State undertakes to reform him - but how and at what cost?',
-    imageLinks: {smallThumbnail: 'http://books.google.com/books/content?id=…er&img=1&zoom=5&edge=curl&source=gbs_api', thumbnail: 'http://books.google.com/books/content?id=qU…cover&img=1&zoom=1&edge=curl&source=gbs_api'},
-    infoLink: 'https://play.google.com/store/books/details?id=qUI8pbpCNJUC&source=gbs_api',
-    language: 'en',
-    maturityRating: 'NOT_MATURE',
-    pageCount: 176,
-    panelizationSummary: {containsEpubBubbles: false, containsImageBubbles: false},
-    previewLink: 'http://books.google.ca/books?id=qUI8pbpCNJUC&printsec=frontcover&dq=A+Clockwork+Orange&hl=&cd=1&source=gbs_api',
-    printType: 'BOOK',
-    publishedDate: '2011-08-04',
-    publisher: 'Penguin UK',
-    title: 'A Clockwork Orange'
-}
-
-const moviePayload = {
-    Actors: "Malcolm McDowell, Patrick Magee, Michael Bates",
-    Awards: "Nominated for 4 Oscars. 12 wins & 24 nominations total",
-    BoxOffice: "$26,617,553",
-    Country: "United Kingdom, United States",
-    DVD: "23 Oct 2007",
-    Director: "Stanley Kubrick",
-    Genre: "Crime, Sci-Fi",
-    Language: "English",
-    Metascore: "77",
-    Plot: "In the future, a sadistic gang leader is imprisoned and volunteers for a conduct-aversion experiment, but it doesn't go as planned.",
-    Poster: "https://m.media-amazon.com/images/M/MV5BMTY3MjM1Mzc4N15BMl5BanBnXkFtZTgwODM0NzAxMDE@._V1_SX300.jpg",
-    Production: "N/A",
-    Rated: "X",
-    Ratings: [
-        {
-            Source: 'Internet Movie Database',
-            Value: '8.3/10'
-        },
-        {
-            Source: 'Rotten Tomatoes',
-            Value: '87%'
-        },
-        {
-            Source: 'Metacritic',
-            Value: '77/100'
-        }
-    ],
-    Released: "02 Feb 1972",
-    Response: "True",
-    Runtime: "136 min",
-    Title: "A Clockwork Orange",
-    Type: "movie",
-    Website: "N/A",
-    Writer: "Stanley Kubrick, Anthony Burgess",
-    Year: "1971",
-    imdbID: "tt0066921",
-    imdbRating: "8.3",
-    imdbVotes: "810,908"
-};
 
 const movieOptions = {
 	method: 'GET',
